43082f87ac70ff99fee0401b77964e0e0f4666c9
danielopq <danielopq@gmail.com>
2024-08-04 17:37:48 +0100

    insure.js - code cleaning JSDoc added

--- js/insure.js
+++ js/insure.js
@@ -1,9 +1,16 @@
-window.onload = start;
+/**
+ * @fileoverview Contains de main actions do hide or display the main menu while the mobile version is running.
+ * @author Daniel Martinez Duque
+ * @date 2024-08-04
+ */
 
-function start(){
+(()=>{
 	document.getElementById("nav-hamburger").addEventListener('click',showMobileMenu,false);
-}
+})()
 
+/**
+ * Display or hide the menu and swap the icon button depending on whether the menu is deployed or not.
+ */
 function showMobileMenu(){
 	if(document.getElementById("nav-mobile").style.display == "none"){
 		document.getElementById("nav-hamburger").style.backgroundImage = "url('images/icon-close.svg')";
